1aac0488d3aecaf599f628e388a65502fbd07a71
Dean <deanlo@aol.com>
2026-07-11 16:12:01 -0400

    work in progress fetched pet data

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 async function start() {
-    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-    const weatherData = await weatherPromise.json
-    const ourTemperature = weatherData.properties.periods[0].temperature
-    document.querySelector("#temperature-ouput").textContent = ourTemperature
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
 }
-console.log ("Hello")
+
 
 
 start()
 
+async function petArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petData = await petsPromise.json()
+  petData.forEach(pet => {
+    console.log(pet.name)
+  })
+}
+petArea()
